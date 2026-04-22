2378109893ca63cf6c4c0c10b5d72cbc04f14895
Texcajun72 <michael@bourgeois.com>
2026-04-22 18:24:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import plotly.graph_objects as go
 import streamlit as st
 
 st.set_page_config(
-    page_title="Partner Enablement Program Dashboard",
+    page_title="Partner Enablement Program Dashboard for 2025",
     page_icon="📊",
     layout="wide",
     initial_sidebar_state="collapsed",
